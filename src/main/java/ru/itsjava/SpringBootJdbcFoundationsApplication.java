3c6479a064c82616b6a3be605e269e94ba22d97a
Dmitr Tkachenko <mitja-tkachenk@rambler.ru>
2022-03-14 14:01:38 +0300

    Homework test

--- src/main/java/ru/itsjava/SpringBootJdbcFoundationsApplication.java
+++ src/main/java/ru/itsjava/SpringBootJdbcFoundationsApplication.java
@@ -13,27 +13,7 @@ import java.sql.SQLException;
 @SpringBootApplication
 public class SpringBootJdbcFoundationsApplication {
     public static void main(String[] args) throws SQLException {
-        ApplicationContext context = SpringApplication.run(SpringBootJdbcFoundationsApplication.class, args);
-
-        //Console.main(args);
-
-        UserDao userDao = context.getBean(UserDao.class);
-
-        System.out.println("userDao.count() = " + userDao.count());
-
-        User user = new User("Kola", 41);
-        userDao.insert(user);
-
-        System.out.println("userDao.count() = " + userDao.count());
-
-        User updateUser = new User("PAVELITEL", 300);
-        updateUser.setId(2L);
-        userDao.update(updateUser);
-
-        System.out.println("userDao.findById(2L) = " + userDao.findById(2L));
-
-        userDao.delete(updateUser);
-        System.out.println("userDao.count() = " + userDao.count());
+        SpringApplication.run(SpringBootJdbcFoundationsApplication.class, args);
 
         Console.main(args);
     }
